32d65f264f7564648a1ff837623c6b5e506aea20
Hokage <oyebolatomilade@gmail.com>
2025-07-01 16:00:08 +0100

    Added cors to index file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
+// CORS headers
+header("Access-Control-Allow-Origin: http://localhost:8081");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 session_start();
+
+
+
+
 
 // --- 1. Normalize Request URI ---
 $method = $_SERVER['REQUEST_METHOD'];
