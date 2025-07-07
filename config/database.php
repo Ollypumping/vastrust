27f2d8ebd3bb218283da24309ed0265e7fdc010c
Hokage <oyebolatomilade@gmail.com>
2025-07-07 13:02:18 +0100

    Addded online db fixed routing

--- config/database.php
+++ config/database.php
@@ -5,10 +5,20 @@ class Database {
 
     public static function connect() {
         if (!self::$conn) {
+            // Load environment variables from .env file
+            $env = parse_ini_file(__DIR__ . '/../.env');
+
+            $db_host = $env['MYSQL_ADDON_HOST'];
+            $db_name = $env['MYSQL_ADDON_DB'];
+            $db_user = $env['MYSQL_ADDON_USER'];
+            $db_pass = $env['MYSQL_ADDON_PASSWORD'];
+            $db_port = isset($env['MYSQL_ADDON_PORT']) ? (int)$env['MYSQL_ADDON_PORT'] : 3306;
+
+            $dsn = "mysql:host=$db_host;dbname=$db_name;port=$db_port;charset=utf8mb4";
             self::$conn = new \PDO(
-                "mysql:host=localhost;dbname=vastrust;charset=utf8mb4", 
-                "root", 
-                ""
+                $dsn,
+                $db_user,
+                $db_pass
             );
             self::$conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         }
